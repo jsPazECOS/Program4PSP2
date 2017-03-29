ee2c96eb74d7cd0e4042d77300e63a74094e9a71
Juan Paz <juan.paz@safetech.com.co>
2017-03-29 18:47:05 -0500

    Cambios2

--- src/main/java/edu/uniandes/ecos/CAIS/P4PSP2/view/App.java
+++ src/main/java/edu/uniandes/ecos/CAIS/P4PSP2/view/App.java
@@ -16,6 +16,7 @@ public class App {
         programa4.readFiles(path);
         programa4.showResults();
         
-     //   get("/", (req, res) -> "Hello World");
+        port(Integer.valueOf(System.getenv("PORT")));
+        get("/", (req, res) -> "Hello World");
     }
 }
